1614f26b77124a6dd881f4da27f2e932d3c4836c
Alexey Dubkov <alexey.dubkov@gmail.com>
2018-01-06 17:24:54 -0800

    init commit

---
+++ MagicLight/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MagicLight
+//
+//  Created by Alexey Dubkov on 1/6/18.
+//  Copyright © 2018 Alexey Dubkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
